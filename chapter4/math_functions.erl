6a324fc9fa1f40c1e7bef9876a0f645170deb079
Paolo Laurenti <laurentipaolo@gmail.com>
2016-05-17 14:16:10 +0200

    Complete exercises of chapter 4

--- chapter4/math_functions.erl
+++ chapter4/math_functions.erl
@@ -1,5 +1,5 @@
 -module (math_functions).
--export([even/1, odd/1, filter/2]).
+-export([even/1, odd/1, filter/2, splitf/1,  splita/1]).
 
 even(N) when N rem 2 =:= 0 -> true;
 even(_) -> false.
@@ -8,3 +8,14 @@ odd(N) when N rem 2 =/= 0 -> true;
 odd(_) -> false.
 
 filter(F, L) -> [X || X <- L, F(X) =:= true].
+
+splitf(L) -> splitf_acc(L, [], []).
+
+splitf_acc([], E, O) -> {lists:reverse(E), lists:reverse(O)};
+splitf_acc([H|T], E, O) ->
+  case even(H) of
+    true -> splitf_acc(T, [H|E], O);
+    false -> splitf_acc(T, E, [H|O])
+  end.
+
+splita(L) -> { filter(fun even/1, L), filter(fun odd/1, L)}.
