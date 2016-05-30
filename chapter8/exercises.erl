3931fc3cbae0b821786b6a65c4c65111a3c243db
Paolo Laurenti <laurentipaolo@gmail.com>
2016-05-30 17:50:49 +0200

    Complete second part of second exercises of Chapter 8

--- chapter8/exercises.erl
+++ chapter8/exercises.erl
@@ -8,16 +8,26 @@
 %%%-------------------------------------------------------------------
 -module(exercises).
 -author("paolo").
--export([get_module_with_most_functions/0]).
+-export([get_module_with_most_functions/0, get_most_common_function_name_in_modules/0]).
 
 
 get_module_with_most_functions() ->
-  Modules = code:all_loaded(),
-  Map = lists:map(fun get_functions_count/1, Modules),
+  Map = lists:map(fun({ModuleName, _}) -> get_functions_count(ModuleName) end, code:all_loaded()),
   [Max | _] = lists:sort(fun({_, C1}, {_, C2}) -> C1 > C2 end, Map),
   Max.
 
-get_functions_count({ModuleName, _}) ->
+get_functions_count(ModuleName) ->
+  {ModuleName, length(get_module_functions(ModuleName))}.
+
+get_module_functions(ModuleName) ->
   Info = erlang:apply(ModuleName, module_info, []),
   [{_, Functions} | _] = lists:filter(fun({InfoType, _}) -> InfoType =:= exports end, Info),
-  {ModuleName, length(Functions)}.
+  Functions.
+
+get_most_common_function_name_in_modules() ->
+  Functions = lists:flatmap(fun({ModuleName, _}) -> get_module_functions(ModuleName) end, code:all_loaded()),
+  FunctionsNames = lists:map(fun({Name, _}) -> Name end, Functions),
+  %lists:foldl(fun(X, Acc) -> dict:update(X, fun(Old) -> Old + 1 end, 1, Acc) end, dict:new(), FunctionsNames),
+  FunctionsOccurrences = lists:foldl(fun(X, Acc) -> dict:update_counter(X, 1, Acc) end, dict:new(), FunctionsNames),
+  [H | _] = dict:fold(fun(Key, Value, AccIn) -> [{_, CurrentMax} | _] = AccIn, case CurrentMax < Value of true ->[{Key, Value} | AccIn]; false -> AccIn end end, [{"", 0}], FunctionsOccurrences),
+  H.
