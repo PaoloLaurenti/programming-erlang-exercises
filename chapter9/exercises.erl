7db5bae4c69ab40f581ea3dbc641a7a63a8a6fa2
Paolo Laurenti <laurentipaolo@gmail.com>
2016-06-01 17:54:28 +0200

    Complete first exercise of chapter 9. Start second exercise

---
+++ chapter9/exercises.erl
@@ -0,0 +1,23 @@
+-module(exercises).
+-author("paolo").
+-export([fac/1, area/1]).
+
+-spec fac(integer()) -> integer().
+fac(0) -> 1;
+fac(N) -> N*fac(N-1).
+
+%% SPEFICATION 1
+%%-type shapeName() :: rectangle | square | circle | right_angled_triangle.
+%%-type shape() :: {shapeName(), pos_integer()} | {shapeName(), pos_integer(), pos_integer()}.
+%%-spec area(shape()) -> integer().
+
+%% SPECIFICATION 2
+%%-spec area(Shape) -> pos_integer() when
+%%  Shape :: {ShapeName,  pos_integer()} | {ShapeName,  pos_integer(),  pos_integer()},
+%%  ShapeName :: rectangle | square | circle | right_angled_triangle.
+
+%area({rectangle, Width, Height, UUU}) -> Width * Height * UUU;
+area({rectangle, Width, Height}) -> Width * Height;
+area({square, Side}) -> Side * Side;
+area({circle, Radius}) -> math:pi() * Radius * Radius;
+area({right_angled_triangle, Width, Height}) ->  (Width * Height) / 2.
