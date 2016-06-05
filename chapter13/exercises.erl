0aab7fc8ac3a047513e4fa11c4fd7eeb1661a6f3
Paolo Laurenti <laurentipaolo@gmail.com>
2016-06-05 19:34:56 +0200

    Complete second exercise of Chapter 13

--- chapter13/exercises.erl
+++ chapter13/exercises.erl
@@ -8,7 +8,7 @@
 %%%-------------------------------------------------------------------
 -module(exercises).
 -author("paolo").
--export([my_spawn/3, do_nothing/1]).
+-export([my_spawn/3, do_nothing/1, my_spawn2/3]).
 
 my_spawn(Mod, Fun, Args) ->
   spawn(fun() ->
@@ -29,3 +29,21 @@ do_nothing(Time) ->
     false
   end.
 
+on_exit(Pid, Fun) ->
+  spawn(fun() ->
+          Ref = monitor(process, Pid),
+          receive
+            {'DOWN', Ref, process, Pid, Why} ->
+              Fun(Why)
+          end
+        end).
+
+my_spawn2(Mod, Fun, Args) ->
+  Pid = spawn(Mod, Fun, Args),
+  Start = erlang:monotonic_time(),
+  on_exit(Pid, fun(Why) ->
+                End = erlang:monotonic_time(),
+                io:format("Process ~p died due to: ~p~n", [Pid, Why]),
+                io:format("Duration: ~p~n", [erlang:convert_time_unit(End - Start, native, seconds)])
+               end),
+  Pid.
