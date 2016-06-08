46a0afd3e772612c440ca87ac24e7537a52824d8
Paolo Laurenti <laurentipaolo@gmail.com>
2016-06-08 14:20:11 +0200

    Complete fifth exercise of chapter 13

---
+++ chapter13/multi_monitor.erl
@@ -0,0 +1,48 @@
+%%%-------------------------------------------------------------------
+%%% @author paolo
+%%% @copyright (C) 2016, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 07. Jun 2016 13.27
+%%%-------------------------------------------------------------------
+-module(multi_monitor).
+-author("paolo").
+-export([spawn_all/1, start/1, do_something/1, spawn_monitored_processes/1]).
+
+start(N) ->
+  spawn(multi_monitor, spawn_all, [N]).
+
+spawn_all(N) ->
+  case N > erlang:system_info(process_limit) of
+    true ->
+      exit(self(), too_many_processes_requested);
+    false ->
+      spawn(multi_monitor, spawn_monitored_processes, [N])
+  end.
+
+spawn_monitored_processes(N) ->
+  Processes = [spawn_monitored_process() || _ <- lists:seq(1, N)],
+  monitor_all(Processes).
+
+monitor_all(Processes) ->
+  receive
+    {'DOWN', Ref, process, Pid, _Why} ->
+      io:format("Process {~p, ~p} terminated and restarted~n", [Pid, Ref]),
+      NewProcessesCatalog = lists:delete({Pid, Ref}, Processes),
+      NewProcess = spawn_monitored_process(),
+      monitor_all([NewProcess | NewProcessesCatalog]);
+    shutdown ->
+      lists:foreach(fun({P, R}) -> demonitor(R) andalso exit(P, shutdown) end, Processes),
+      io:format("All processes terminated~n", [])
+  end.
+
+spawn_monitored_process() ->
+  spawn_monitor(multi_monitor, do_something, [10000]).
+
+do_something(Time) ->
+  receive
+    _ -> true
+  after Time ->
+    false
+  end.
