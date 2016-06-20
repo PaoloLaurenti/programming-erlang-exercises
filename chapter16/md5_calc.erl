7315edb6d7cc041dcbcba11f4cb96763f4447391
Paolo Laurenti <laurentipaolo@gmail.com>
2016-06-20 14:09:50 +0200

    Complete third exercise of Chapter 16

--- chapter16/md5_calc.erl
+++ chapter16/md5_calc.erl
@@ -10,8 +10,23 @@
 -author("paolo").
 
 %% API
--export([calc_md5/1]).
+-export([calc_md5/1, calc_md5_large/1]).
 
 calc_md5(File) ->
   {ok, Content} = file:read_file(File),
   erlang:md5(Content).
+
+calc_md5_large(File) ->
+  Context = erlang:md5_init(),
+  {ok, F} = file:open(File, [read,binary,raw]),
+  calc_chunk(F, Context, 1),
+  erlang:md5_final(Context).
+
+calc_chunk(File, Context, Location) ->
+  case file:pread(File, Location, 100) of
+    {ok, Chunk} ->
+      erlang:md5_update(Context, Chunk),
+      calc_chunk(File, Context, Location + 1000);
+    eof ->
+      ok
+  end.
