8b619be8898234c3578ace4e3b7930e0812b5fca
Paolo Laurenti <laurentipaolo@gmail.com>
2016-06-23 23:16:00 +0200

    Attempt to solve fourth exercice of Chapter 16

---
+++ chapter16/image_finder.erl
@@ -0,0 +1,25 @@
+%%%-------------------------------------------------------------------
+%%% @author paolo
+%%% @copyright (C) 2016, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 23. Jun 2016 21:53
+%%%-------------------------------------------------------------------
+-module(image_finder).
+-author("paolo").
+-export([get_identical_images/1, get_images/1]).
+-import(lib_find, [files/3, files/5]).
+
+get_identical_images(Dir) ->
+  Map = files(Dir, "*.jpg", true, fun index_image/2, dict:new()),
+  Identical = dict:filter(fun(_Key, Value) -> length(Value) > 1 end, Map),
+  lists:map(fun({_Key, Val}) -> Val end ,dict:to_list(Identical)).
+
+index_image(Image, ImagesMap) ->
+  ImageMd5 = md5_calc:calc_md5_large(Image),
+  dict:update(binary_to_list(ImageMd5), fun(V) -> [Image|V] end, [Image], ImagesMap).
+
+get_images(Dir) ->
+  F = files(Dir, "*.jpg", true),
+  lists:map(fun(I) -> binary_to_list(md5_calc:calc_md5(I)) end, F).
